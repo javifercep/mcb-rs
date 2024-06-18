d619913a207bcd469ab65cd331444d16564ee409
Javier Fernández <j.fer.cep@gmail.com>
2024-06-18 19:39:42 +0200

    Improving my understanding of rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,236 @@
 #![crate_type = "lib"]
+#![crate_name = "mcb"]
 
-#![crate_name = "mcb_rust"]
-
-const CFG_EXT_BIT:  u16 = 0x0001;
+const CFG_EXT_BIT: u16 = 0x0001;
 const CFG_STD_READ: u16 = 0x0002;
-const CFG_EXT_READ: u16 = CFG_STD_READ + CFG_EXT_BIT; /// 0x0003
+const CFG_EXT_READ: u16 = CFG_STD_READ + CFG_EXT_BIT;
+/// 0x0003
 const CFG_STD_WRITE: u16 = 0x0004;
-const CFG_EXT_WRITE: u16 = CFG_EXT_READ + CFG_EXT_BIT; /// 0x0005
-const CFG_STD_ACK:   u16 = 0x0006;
-const CFG_EXT_ACK:   u16 = CFG_STD_ACK + CFG_EXT_BIT; /// 0x0007
-const CFG_IDLE:      u16 = 0x000E;
+const CFG_EXT_WRITE: u16 = CFG_EXT_READ + CFG_EXT_BIT;
+/// 0x0005
+const CFG_STD_ACK: u16 = 0x0006;
+const CFG_EXT_ACK: u16 = CFG_STD_ACK + CFG_EXT_BIT;
+/// 0x0007
+const CFG_IDLE: u16 = 0x000E;
+
+pub enum IntfResult {
+    Success,
+    Ready,
+}
 
 #[derive(Debug)]
-pub enum OperationResult{
-    Success,
-    Fail,
+pub enum IntfError {
+    Interface,
+    Access,
 }
 
-pub struct McbData{
-    pub data: u64,
+#[derive(Clone, Copy)]
+pub struct Frame {
+    address: u16,
+    command: u16,
+    raw: [u16; 7],
+    crc: u16,
 }
 pub trait PhysicalInterface {
-    fn raw_write(&self) -> OperationResult {return OperationResult::Success}
-    fn raw_read(&self) -> OperationResult {return OperationResult::Success}
+    fn initialize(&self) -> Result<IntfResult, IntfError> {
+        panic!("Implementation of physical interface is required");
+    }
+    fn raw_write(&self, frame: &Frame) -> Result<IntfResult, IntfError> {
+        panic!("Implementation of physical interface is required");
+    }
+    fn raw_read(&self, frame: &Frame) -> Result<IntfResult, IntfError> {
+        panic!("Implementation of physical interface is required");
+    }
 }
 
-impl PhysicalInterface for McbData {}
-
-/// Initialize the library.
-///
-/// This function is required in systems where device peripherals requires
-/// a specific configuration. For example, when the MCB protocol is 
-/// implemented over SPI interfaces. A false result of this function
-/// must be considered as critical error
-/// 
-/// ```rust
-/// if mcb_rust::init() == false {
-///     panic!("Library cannot be initialized");
-/// }
-/// ```
-pub fn init() -> bool {
-    return true;
+/// Mcb State interface
+pub struct Master<STATE, INTERFACE: PhysicalInterface> {
+    frame: Frame,
+    state: STATE,
+    interface: INTERFACE,
 }
 
-pub fn write(add: u16, data: McbData) -> OperationResult {
-    let mut raw_frame: [u16; 6] = [0; 6];
-    let test: [u16; 4];
-
-    raw_frame[0] = CFG_STD_WRITE + (add << 4);
-    test = bytemuck::cast(data.data);
-    raw_frame[0..4].copy_from_slice(&test);
-    raw_frame[5] = 0u16;
-
-    return data.raw_write();
+pub struct Slave<STATE, INTERFACE: PhysicalInterface> {
+    frame: Frame,
+    state: STATE,
+    interface: INTERFACE,
 }
 
-pub fn read(add: u16, data: McbData) -> OperationResult {
-    let mut raw_frame: [u16; 6] = [0; 6];
-    let test: [u16; 4];
+// Type States for MODE in State
+pub struct Init;
+pub struct Config;
+pub struct Cyclic;
 
-    raw_frame[0] = CFG_STD_READ + (add << 4);
-    test = bytemuck::cast(data.data);
-    raw_frame[0..4].copy_from_slice(&test);
-    raw_frame[5] = 0u16;
-
-    return data.raw_read();
+/// These functions may be used on any Mcb struct
+impl<INTF> Master<Init, INTF>
+where
+    INTF: PhysicalInterface,
+{
+    pub fn init(self) -> Master<Config, INTF> {
+        Master {
+            frame: self.frame,
+            state: Config,
+            interface: self.interface,
+        }
+    }
 }
 
-pub fn enable_cyclic() -> OperationResult {
-    return OperationResult::Success;
+/// These functions may be used on any Mcb in config State
+impl<INTF> Master<Config, INTF>
+where
+    INTF: PhysicalInterface,
+{
+    pub fn writeu8(&mut self, add: u16, data: u8) -> Result<IntfResult, IntfError> {
+        self.frame.raw[0] = self.frame.address;
+        self.frame.raw[1] = CFG_STD_WRITE + (add << 4);
+        self.frame.raw[2] = data as u16;
+        self.frame.raw[6] = 0u16;
+
+        self.interface.raw_write(&self.frame)
+    }
+
+    pub fn readu8(&mut self, add: u16) -> Result<IntfResult, IntfError> {
+        self.frame.raw[0] = self.frame.address;
+        self.frame.raw[1] = CFG_STD_READ + (add << 4);
+        self.frame.raw[6] = 0u16;
+
+        return self.interface.raw_read(&self.frame);
+    }
+
+    pub fn into_cyclic(self) -> Master<Cyclic, INTF> {
+        Master {
+            frame: self.frame,
+            state: Cyclic,
+            interface: self.interface,
+        }
+    }
 }
 
-pub fn disable_cyclic() -> OperationResult {
-    return OperationResult::Success;
+/// These functions may be used on any Mcb in config State
+impl<INTF> Master<Cyclic, INTF>
+where
+    INTF: PhysicalInterface,
+{
+    pub fn write(&mut self, add: u16) -> Result<IntfResult, IntfError> {
+        return self.interface.raw_write(&self.frame);
+    }
+
+    pub fn read(&mut self, add: u16) -> Result<IntfResult, IntfError> {
+        return self.interface.raw_read(&self.frame);
+    }
+
+    pub fn into_config(self) -> Master<Config, INTF> {
+        Master {
+            frame: self.frame,
+            state: Config,
+            interface: self.interface,
+        }
+    }
+}
+
+/// These functions may be used on any Mcb struct
+impl<STAT, INTF> Slave<STAT, INTF>
+where
+    INTF: PhysicalInterface,
+{
+    pub fn init(self) -> Slave<Config, INTF> {
+        Slave {
+            frame: self.frame,
+            state: Config,
+            interface: self.interface,
+        }
+    }
+}
+
+/// These functions may be used on any Mcb in config State
+impl<INTF> Slave<Config, INTF>
+where
+    INTF: PhysicalInterface,
+{
+    pub fn writeu8(&mut self, add: u16, data: u8) -> Result<IntfResult, IntfError> {
+        self.frame.raw[0] = self.frame.address;
+        self.frame.raw[1] = CFG_STD_ACK + (add << 4);
+        self.frame.raw[2] = data as u16;
+        self.frame.raw[6] = 0u16;
+
+        return self.interface.raw_write(&self.frame);
+    }
+
+    pub fn readu8(&mut self, add: u16) -> Result<IntfResult, IntfError> {
+        self.frame.raw[0] = self.frame.address;
+        self.frame.raw[1] = CFG_IDLE;
+        self.frame.raw[6] = 0u16;
+
+        return self.interface.raw_read(&self.frame);
+    }
+
+    pub fn listen(&self) -> Result<IntfResult, IntfError> {
+        Ok(IntfResult::Success)
+    }
+
+    pub fn into_cyclic(self) -> Slave<Cyclic, INTF> {
+        Slave {
+            frame: self.frame,
+            state: Cyclic,
+            interface: self.interface,
+        }
+    }
+}
+
+/// These functions may be used on any Mcb in config State
+impl<INTF> Slave<Cyclic, INTF>
+where
+    INTF: PhysicalInterface,
+{
+    pub fn write(&mut self, add: u16) -> Result<IntfResult, IntfError> {
+        return self.interface.raw_write(&self.frame);
+    }
+
+    pub fn read(self, add: u16) -> Result<IntfResult, IntfError> {
+        return self.interface.raw_read(&self.frame);
+    }
+
+    pub fn into_config(self) -> Slave<Config, INTF> {
+        Slave {
+            frame: self.frame,
+            state: Config,
+            interface: self.interface,
+        }
+    }
+}
+
+pub fn create_master_mcb<INTF: PhysicalInterface>(interface: Option<INTF>) -> Master<Init, INTF> {
+    let interface_in = interface.unwrap();
+    match interface_in.initialize() {
+        Err(e) => panic!("Interface is not initialized because of error: {:?}", e),
+        _ => (),
+    }
+    Master {
+        frame: Frame {
+            address: 0u16,
+            command: 0u16,
+            raw: [0u16; 7],
+            crc: 0u16,
+        },
+        state: Init,
+        interface: interface_in,
+    }
+}
+
+pub fn create_slave_mcb<INTF: PhysicalInterface>(interface: Option<INTF>) -> Slave<Init, INTF> {
+    let interface_in = interface.unwrap();
+    match interface_in.initialize() {
+        Err(e) => panic!("Interface is not initialized because of error: {:?}", e),
+        _ => (),
+    }
+    Slave {
+        frame: Frame {
+            address: 0u16,
+            command: 0u16,
+            raw: [0u16; 7],
+            crc: 0u16,
+        },
+        state: Init,
+        interface: interface_in,
+    }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,18 +1,92 @@
-use mcb_rust::{OperationResult, McbData};
+use mcb::{
+    create_master_mcb, create_slave_mcb, Init, Master, Slave, 
+    Frame, PhysicalInterface, IntfResult, IntfError, 
+};
 
-#[test]
-fn test_initialization() {
-    assert_eq!(mcb_rust::init(), true);
+use mcb::{
+    IntfResult::*,
+    IntfError::*,
+};
+
+use std::{
+    sync::mpsc, sync::mpsc::Sender, sync::mpsc::Receiver
+};
+use std::thread;
+
+struct SlaveThread<T> {
+    tx_channel: Sender<T>,
+    rx_channel: Receiver<T>,
+}
+struct MasterThread<T>{
+    tx_channel: Sender<T>,
+    rx_channel: Receiver<T>,
+}
+
+fn create_masterslavethread<T>() -> (SlaveThread<T>, MasterThread<T>) {
+    let (mtx, mrx) = mpsc::channel();
+    let (stx, srx) = mpsc::channel();
+
+    (SlaveThread {
+        tx_channel: stx,
+        rx_channel: mrx,
+    },
+     MasterThread {
+        tx_channel: mtx,
+        rx_channel: srx,
+     })
+
+
+}
+
+impl PhysicalInterface for SlaveThread<Frame> {
+    fn initialize(&self) -> Result<IntfResult, IntfError> {
+        Ok(Success)
+    }
+    fn raw_write(&self, frame: &Frame) -> Result<IntfResult, IntfError>  {
+        Ok(Success)
+    }
+    fn raw_read(&self, frame: &Frame) -> Result<IntfResult, IntfError>  {
+        match self.rx_channel.recv().unwrap() {
+            _ => Ok(Success),
+        }
+    }
+}
+
+impl PhysicalInterface for MasterThread<Frame> {
+    fn initialize(&self) -> Result<IntfResult, IntfError> {
+        Ok(Success)
+    }
+    fn raw_write(&self, frame: &Frame) -> Result<IntfResult, IntfError>  {
+        let msg = frame.clone();
+        self.tx_channel.send(msg).unwrap();
+        Ok(Success)
+    }
+    fn raw_read(&self, frame: &Frame) -> Result<IntfResult, IntfError>  {
+        Ok(Success)
+    }
 }
 
 #[test]
-fn test_read(){
-    let reg:  McbData ={McbData{data: 0u64}};
-    let result: bool;
+fn test_read() {
+    let (slave_thread, master_thread) = create_masterslavethread();
+    let mcb_slave_test: Slave<Init, SlaveThread<Frame>> = create_slave_mcb(Some(slave_thread));
 
-    match mcb_rust::read(0u16, reg) {
-        OperationResult::Success => result = true,
-        OperationResult::Fail => result = false,
+    thread::spawn(move || {
+        let mcb_master_test: Master<Init, MasterThread<Frame>> = create_master_mcb(Some(master_thread));
+        let mut mcb_master_cfg = mcb_master_test.init();
+        mcb_master_cfg.writeu8(0u16, 0u8);
+        // commmented code to check test is working
+    });
+
+    let mut mcb_slave_cfg = mcb_slave_test.init();
+
+    let mut result;
+
+    match mcb_slave_cfg.readu8(0u16) {
+        Ok(Success) => result = true,
+        Err(Interface) => result = false,
+        _ => result = false,
     }
+
     assert_eq!(result, true);
 }
